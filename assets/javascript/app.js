6d7b1ad61a1fc4e086dfd008faac1af4e310605e
betonit007 <timnagorski@hotmail.com>
2018-10-06 17:47:21 -0400

    done with timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,55 @@
 ////Variables//
 var timeleft;
+var timer;
 
+///////////////////////////////////////
+start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////functions////////////////////////////////////////
 
 ////Timer/////
-timeleft = 30;
+function thirtySecTimer() {
+    timeleft = 11;
+    timer = setInterval(function(){
+        $("#timer").text("You have: " + timeleft + " seconds remaining.");
+        timeleft--;
+        if (timeleft < 10) {
+            $("#timer").css("color", "red");
+        }
+        if (timeleft < 0) 
+        {
+        $("#timer").css("color", "#D7A84F");
+        $("#timer").text("Time is Up!");
+        }
+    }, 1000);
+}
 
-$("#timer").text(timeleft);
+function start() {
+    $("#start").text("Start");
+    //$("#start").attr("class", "answer");
+}
+    
+
   
 
 
