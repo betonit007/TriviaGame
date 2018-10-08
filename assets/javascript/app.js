8ddbad36480218662e99785d22ac6e2d27f8e453
betonit007 <timnagorski@hotmail.com>
2018-10-08 08:36:23 -0400

    working on random answer placement

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 ////Variables//
 var timeleft;
 var timer;
-
+var counter = 0;
+var f = 2;
 ///////////////////////////////////////
 
 ////////////////////////////////////////////////////////Main Body//////////////////////////////////////////
@@ -59,11 +60,28 @@ function start() {
     
 function possibleAnswers() {
     $(".aField").addClass("answer");
+    $("#answer" + f).text(questonAnswerArray[0].correctAnswer);
 }
 
 function questions() {
     $("#question").text("What will be the first Question in Saab Trivia?");
 }
+
+////////////creates question / answer info arrays/////////////////////////
+
+function qAndA(saabQuestion, correctAnswer) {
+   this.saabQuestion = saabQuestion;
+   this.correctAnswer = correctAnswer;
+   this.saabAnswers = [];
+   
+
+}
+
+var questionAnswer1 = new qAndA("What is the fastest Saab Protection car, 0-60 mph?", "1999 9-3 Viggen");
+questionAnswer1.saabAnswers[0] = "2008 9-3 Turbo X SportCombi";
+questionAnswer1.saabAnswers[1] = "2006 9-3 Aero V6";
+
+var questonAnswerArray = [questionAnswer1];
 
 
 
